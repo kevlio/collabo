56dfad2f596a71fcbc9c77ba077f73f48c756a37
Marcus Piri <Marcus.piri@cmeducations.se>
2022-04-25 13:01:48 +0200

    untzz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-
+import "../home.css";
 function Home() {
 	return (
 		<>
-			<h1>FEA2021 Only Fans</h1>
-			<h3>Hungry for some smexi code?</h3>
-			<p>Check out this hot as P tag</p>
-			<div>How about this fine div ??</div>
+			<div>
+				<h1>FEA2021 Only Fans</h1>
+				<h3>Hungry for some smexi code?</h3>
+				<p>Check out this hot as P tag</p>
+				<div>How about this fine div ??</div>
+			</div>
+			<div className="btn">
+				<p>Wanna see me live code for u ?</p>
+				<button>Click here!</button>
+			</div>
 		</>
 	);
 }
