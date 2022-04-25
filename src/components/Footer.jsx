3eb108dbe711b0b3ec709ae22295d59c1cb722ef
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-04-25 12:27:57 +0200

    really ugly footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Footer() {
+  const style = {
+      backgroundImage: `url(https://media.istockphoto.com/photos/green-grass-picture-id480560906?k=20&m=480560906&s=170667a&w=0&h=2DEj7n-7GMnkkV1vM-9aAr88S3iwOEzuYagZda99AG4=)`,
+      backgroundSize: `cover`,
+      margin: "0px",
+      fontSize: "1rem",
+      padding: "1rem",
+      textAlign: "center",
+      color: "rgba(255, 255, 255, 0.66)"
+  };
+
+  return (
+    <div>
+      <h3 style={style}>(¯`·.·´¯) <br />
+. · . · . ·°·.¤.·°· . · . · . <br />
+(¯`·.¸.•°*°•. @ .•°*°•.¸.·´¯) <br />
+·.·°·.¨¨*~BR5~*¨¨.·°·.· <br />
+. · . · . ·°·.¤.·°· . · . · . <br />
+° º @ º ° <br />
+,(¸.•´¯) <br />
+. .¸.•´ <br />
+(.* <br />
+V <br />
+;</h3>
+    </div>
+  );
+}
+
+export default Footer;
