d0d877a76ef5df12b2bcbf6b039c48e392f53073
Timothy Magee <oaklessons@gmail.com>
2022-04-25 13:49:25 +0200

    projects page now added to router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Maria from "./pages/Maria";
 import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/maria" element={<Maria />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );
