5a5851788b479385e0838dacfaad2478d214b73f
Marcus Piri <Marcus.piri@cmeducations.se>
2022-04-25 12:16:31 +0200

    Some text to get it started

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+	return (
+		<>
+			<h1>FEA2021 Only Fans</h1>
+			<h3>Hungry for some smexi code?</h3>
+			<p>Check out this hot as P tag</p>
+			<div>How about this fine div ??</div>
+		</>
+	);
+}
+
+export default Home;
